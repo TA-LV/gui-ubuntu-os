ad8c9b01cdb267723ffc459a698c2417a207d662
linuxdevoloper <rbasry29@gmail.com>
2026-02-21 10:11:49 +0600

    Update fix-stuck.sh

--- fix-stuck.sh
+++ fix-stuck.sh
@@ -9,11 +9,14 @@ c='\e[1;96m'
 
 
 
-clear
-printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
-printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
-printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
-printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
+ banner() {
+    clear                                                                   # Box is exactly 29 characters wide                                     printf "${b}▛▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▜\n"                            printf "${b}▌${g}█ █ █▀▀█ █ █ █▀▀▄ ▀▀█▀▀ █ █${b}▐\n"
+    printf "${b}▌${g}█ █ █▀▀▄ █ █ █  █   █   █ █${b}▐\n"                    printf "${b}▌${g}▀▀▀ ▀▀▀▀ ▀▀▀ ▀  ▀   ▀   ▀▀▀${b}▐\n"
+    printf "${b}▌                           ${b}▐\n"
+    printf "${b}▌      ${g}GUI-UBUNTU-OS        ${b}▐\n"
+    printf "${b}▙▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▟${r}\n\n"
+}
+banner
 printf "${b}[${g}*${b}]${c} Fixing issue.... ${r}\n"
 
 proot-distro login ubuntu -- dpkg --remove --force-all  plocate
