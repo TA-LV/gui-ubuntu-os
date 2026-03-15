00cda5fb1dbca7716d043f5b7898d918bd944771
Rabeya Ripa <rabeyaripa26@gmail.com>
2026-03-15 15:49:40 +0600

    Update cli.sh

--- cli.sh
+++ cli.sh
@@ -4,16 +4,15 @@ b='\e[34m'     # Blue
 r='\e[0m'      # Reset
 y='\e[1;33m'   # Yellow
 c='\e[1;96m'   # Light cyan
-
+bl="\e[1m"     # bold
+red='\e[1;31m' # Red
 
 
 ubuntu_path="$PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu"
-  #!/bin/bash
+
 
 # Color Definitions
-b='\e[1;34m' # Blue Border
-g='\e[1;32m' # Light Green Text
-r='\e[0m'    # Reset
+
 
 banner() {
     clear
@@ -42,20 +41,22 @@ banner() {
 }
 
 
-info()     { printf "${b}[${g}*${b}] ${c}%s${reset}\n" "$1"; }
-
+info()     { printf "${bl}${b}[${g}*${b}] ${c}%s${r}\n" "$1"; }
+warn()     { printf "${bl}${b}[${red}!${b}] ${red}%s${r}\n" "$1"; }
 
 finish(){
 printf "${b}[${g}*${b}]${g} Ubuntu installed! ${r}\n"
-printf "${b}[${g}*${b}]${g} Run ${y} ubuntu ${g} to get Ubuntu cli ${r}\n"
+printf "${b}[${g}*${b}]${c} Log in : ${y} ubuntu  ${r}\n"
 printf "${b}[${g}*${b}]${g} If you want GUI , Then run ${r}\n"
 printf "${b}[${g}*${b}]${y} ./gui.sh ${r}\n"
+info "Enjoy!"
+info "Have a nice day!"
 }
 
 
 check_ubuntu_installed() {
     if [[ -d "$ubuntu_path" ]]; then
-        printf "${b}[${g}!${b}]${c} Distro already available , skipping progress.${r}\n"
+        warn "Distro already available , skipping progress"
         exit 1
     fi
 }
@@ -63,15 +64,15 @@ spinner() {
     local msg="$1"
     shift
     local cmd="$*"
-    local frames=( "∆___" "_∆__" "__∆_" "___∆" )
+    local frames=( "▶───" "─▶──" "──▶─" "───▶" )
     local i=0
 
     # Start spinner in background
     (
         while :; do
-            printf "\r${c}${msg} [${y}${frames[i]}${c}]"
+            printf "\r${bl}${c}${msg} [${y}${frames[i]}${c}]"
             i=$(( (i + 1) % 4 ))
-            sleep 0.5
+            sleep 0.2
         done
     ) &
     local spin_pid=$!
@@ -86,25 +87,46 @@ spinner() {
 
     # Print final line
     if [ $cmd_status -eq 0 ]; then
-        printf "\r${c}${msg} [${y}∆___${c}] ✅ Done\n"
+        printf "\r${c}${msg} [${y}✅✅${c}] \n"
     else
-        printf "\r${c}${msg} [${y}∆___${c}] ❌ Failed\n"
+        printf "\r${c}${msg} [${y}❌❌${c}]\n"
     fi
 }
+check_dependencies() {
+    spinner "${b}[${g}*${b}]${c} Updating and Upgrading packages..." "yes | apt update ; yes | apt upgrade ; sleep 1"
+
+    info "Checking package dependencies..."
+
+
+    REQUIRED_PACKAGES=("proot-distro" "x11-repo" "termux-x11-nightly" "curl" "wget")
+    for PACKAGE_NAME in "${REQUIRED_PACKAGES[@]}"; do
+        if dpkg -s "$PACKAGE_NAME" &> /dev/null; then
+            printf "${bl}${y}~${g}> ${y}$PACKAGE_NAME ${b}is installed${r}\n\n"
+        else
+            printf "${bl}${y}~${g}> ${y}$PACKAGE_NAME ${b} is not installed.${c} Installing...${r}" ; sleep 1
+            sleep 2
+            apt install -y "$PACKAGE_NAME" &>/dev/null ; printf "${y} Done!!${r}\n\n" || {
+                warn "ERROR: Failed to install ${PACKAGE_NAME}. Exiting."
+                exit 1
+            }
+        fi
+    done
+
+}
 check_internet() {
-    TARGET_URL="https://github.com/rbasry29-blip/gui-ubuntu-os"
+    TARGET_URL="https://github.com/TA-LV/gui-ubuntu-os"
 
     if command -v curl >/dev/null 2>&1; then
         curl -Is --connect-timeout 5 "$TARGET_URL" >/dev/null 2>&1
     elif command -v wget >/dev/null 2>&1; then
         wget -q --spider --timeout=5 "$TARGET_URL"
     else
-        printf "${b}[${g}*${b}]${c} Sorry, wget or curl is required for verification.\n"
+        warn "Sorry, wget or curl is required for verification."
         exit 1
     fi
 
     if [ $? -ne 0 ]; then
-        printf "${b}[${g}*${b}]${c} Sorry, internet connection is needed.\n"
+        warn "Sorry, internet connection is needed."
         exit 1
     fi
 }
@@ -114,37 +136,41 @@ LINES=(
   "    Wish your work will easier"
   "    lets's get started!"
   "    Enjoy gui-ubuntu-os"
-  "    Support : If it helped you,  please give a star to this repo. Your star keeps repo alive."
+  "    Support : If it helped you,"
+  "    * please give a star"
+  "    to this repo. "
+  "    * Your star keeps repo alive."
 )
 
 for t in "${LINES[@]}"; do
   for ((i=0;i<${#t};i++)); do
     printf "${C}${B}${t:i:1}${N}"
-    ((RANDOM%3==0)) && { printf "${G} ${N}\b"; sleep 0.05; }
-    sleep 0.08
+    ((RANDOM%3==0)) && { printf "${G} ${N}\b"; sleep 0.01; }
+    sleep 0.05
   done
   echo
   sleep 2
 done
 printf "$r"
-spinner "${b}[${g}*${b}]${c} Cleaning termux environment${g}....." "dpkg --configure -a ; apt --fix-broken install ; sleep 5"
+check_dependencies
+spinner "${b}[${g}*${b}]${c} Preparing environment${g}....." "dpkg --configure -a ; apt --fix-broken install ; sleep 5"
 sleep 1
 
 
 yes | termux-setup-storage
-spinner "${b}[${g}*${b}]${c} Installing packages${g}....." "yes | apt update  && yes | apt install proot-distro && yes | apt install x11-repo  && yes | apt install termux-x11-nightly && yes | apt install x11-repo "
+
 sleep 1
 
-printf "${b}[${g}*${b}]${g} Installing distro${g}.....${r}\n"
+printf "${b}[${g}*${b}]${c} Installing distro${g}.....${r}\n"
 proot-distro install ubuntu
 banner
 set -e
 # Ask for username (TERMUX)
-printf "${b}[${g}*${b}]${g}"
-read -rp " Enter username: " USERNAME
+printf "${g}█${c}▶${y}"
+read -rp " Enter your ubuntu username: " USERNAME
 # Ask for password (TERMUX, hidden)
-printf "${b}[${g}*${b}]${g}"
-read -rsp " Enter password: " PASSWORD
+printf "${g}█${c}▶${y}"
+read -rp " Create a password: " PASSWORD
 echo
 # Run everything INSIDE Ubuntu
 proot-distro login ubuntu -- bash -c "
@@ -156,7 +182,7 @@ echo \"$USERNAME:$PASSWORD\" | chpasswd
 # Add to sudo group
 usermod -aG sudo \"$USERNAME\"
 "
-printf "${b}[${g}*${b}]${g} User '$USERNAME' created and added to sudo group inside Ubuntu.\n"
+printf "${g}█${c}▶${y} User ${c}'$USERNAME' ${y}created and added to sudo group inside Ubuntu.\n"
 sleep 3
 banner
 cat > $PREFIX/bin/server << 'EOF'
@@ -175,8 +201,8 @@ chmod +x $PREFIX/bin/ubuntu
 }
 banner
 info "Copyright (c) 2026 TA-LV"
-check_internet
-check_ubuntu_installed
+# check_internet
+# check_ubuntu_installed
 printf "$c"
 software
 finish
